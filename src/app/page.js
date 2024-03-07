10a68afd385c72f69ad0b8bac69f9eb41f6bd5d9
yoshikazuuu <febrianojerry@gmail.com>
2024-03-07 20:05:49 +0700

    menambahkan tanggal build

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
+  const date = new Date();
+
   return (
     <div className="flex flex-col font-bold text-5xl h-screen justify-center items-center">
       Halo ibu!
       <p className="font-light text-sm">Ini di internet!</p>
+      <p className="font-light text-sm">{date.toString()}</p>
     </div>
   );
 }
